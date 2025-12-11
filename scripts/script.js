b0c6be094006690d940406da5a54ca079e7a37f6
ar-pathak <pathakarsan@gmail.com>
2025-12-11 20:07:25 +0530

    Updated index.html, script.js, and style.css to remove relative paths and update Swiper slider configuration

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,30 @@ let typed = new Typed('#text', {
     backSpeed: 50,
     loop: true
 });
+
+
+//BRAND SLIDER JS
+
+let swiper = new Swiper('.brandSwiper', {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+        },
+        480: {
+            slidesPerView: 3,
+        },
+        768: {
+            slidesPerView: 4,
+        },
+        1024: {
+            slidesPerView: 5,
+        },
+    }
+});
